fb0753ada8e36eb234c3053cac43819c6641bc7c
Devadasunepala <devadasunepala43@gmail.com>
2024-11-19 21:36:54 +0530

    page added

--- src/App.js
+++ src/App.js
@@ -21,13 +21,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import SignupForm from "./components/signup";
-import Front from "./components/front";
 
 function App() {
    return (
       <Router>
          <div className="App">
-            <Front/>
             <Routes>
                <Route path="/" element={<Home />} />
                <Route path="/signup" element={<SignupForm />} />
